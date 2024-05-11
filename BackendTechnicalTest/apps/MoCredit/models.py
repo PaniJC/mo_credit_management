c499de542f9c7ecd0a788c3f68d3f517b5dcf6bb
codepanijc <juan77camilo@gmail.com>
2024-05-11 17:54:11 -0500

    feat(core): finish all the mandatories activities of the test

--- BackendTechnicalTest/apps/MoCredit/models.py
+++ BackendTechnicalTest/apps/MoCredit/models.py
@@ -43,4 +43,3 @@ class PaymentsDetail(models.Model):
 
 
 
-
--- BackendTechnicalTest/apps/MoCredit/services/payments_services.py
+++ BackendTechnicalTest/apps/MoCredit/services/payments_services.py
@@ -16,94 +16,75 @@ def create_payment(request):
 
             # Obtener el external_id de la solicitud GET
             customer_external_id = data.get('customer_external_id')
-            customer = Customers.objects.get(external_id=customer_external_id)
-
             total_amount = data.get('total_amount')
+            external_id = data.get('external_id')
+            total_debt =0
 
+            customer = Customers.objects.get(external_id=customer_external_id)
             if customer.id is not None:
 
-                    # Obtener todos los préstamos asociados al cliente
-                    loans = Loans.objects.filter(customer_id=customer.id)
+                customer_id = customer.id
 
-                    # Validamos si tiene algun prestamo activo
-                    active_loans = False
+                loans = Loans.objects.filter(customer_id=customer_id)
+
+                for loan in loans:
+                    total_debt += float(loan.outstanding)
+                print(f'El total adeudado por el cliente es {total_debt}')
+
+                for loan in loans:
+                    print(loan.status)
+
+                if total_amount > total_debt:
+                    return JsonResponse({'error': 'El monto a pagar es mayor que la deuda. Pago rechazado.'}, status=200)
+                else:
+                    payment = Payments.objects.create(
+                        external_id=data.get('external_id'),
+                        customer_id=customer,  
+                        total_amount=total_amount,
+                        status=1
+                    )
 
                     for loan in loans:
                         if loan.status == 2:
-                             active_loans = True
-                             break
-                    
-                    
-                    if active_loans == True:
-                        # Como efectivamente tiene prestamos activos, pasamos a validar si el pago excede el monto de la deuda.
-                        print('Como efectivamente tiene prestamos activos, pasamos a validar si el pago excede el monto de la deuda')
-                        total_debt =0.00
-                        for loan in loans:
-                            total_debt += float(loan.outstanding)
+                            outsanding_mount = loan.outstanding
+                            if total_amount >= outsanding_mount:
+                                total_amount -= outsanding_mount
+                                payment_detail = outsanding_mount
+                                outsanding_mount = 0
+                                loan.outstanding = outsanding_mount
+                                loan.status = 4
+                                loan.save()
+                            else:
+                                outsanding_mount -= total_amount
+                                payment_detail = total_amount
+                                loan.outstanding = outsanding_mount
+                                total_amount = 0
+                                loan.status = 2
+                                loan.save()
 
-                        balance = total_amount
-                        if total_amount <= total_debt:
-                            # Como efectivamente tiene prestamos activos, pasamos a validar si el pago excede el monto de la deuda.
-                            print('Lo que va a pagar es menor o igual que la deuda total que tiene.')
-                     
-                            payment = Payments.objects.create(
-                                external_id=data.get('external_id'),
-                                customer_id=customer,  # Asignar el cliente encontrado como clave foránea
-                                total_amount=data.get('total_amount'),
-                                status=1
-                            )
-                            
-                            payments_loan_detail = 0
-                            for loan in loans:
-                                if balance > loan.outstanding:
-                                    payments_loan_detail = loan.outstanding
-                                    loan.outstanding = 0.00
-                                    loan.status = 4
-                                    balance = float(balance) - float(loan.outstanding)
-                                    payment.status = 1
-                                    # Guardar los cambios en la base de datos
-                                    loan.save()
-                                    payment.save()
-
-                                else:
-                                    payments_loan_detail = balance
-                                    loan.outstanding = float(loan.outstanding) - float(balance)
-                                    loan.status = 2
-                                    balance = 0
-                                    payment.status = 1
-
-                                    payment_loan_detail = PaymentsDetail.objects.create(
-                                    amount=payments_loan_detail,
+                            payment_loan_detail = PaymentsDetail.objects.create(
+                                    amount=payment_detail,
                                     loan_id=loan,
                                     payment_id=payment,  # Asignar el cliente encontrado como clave foránea
                                     )
-                                    # Guardar los cambios en la base de datos
-                                    loan.save()
-                                    payment.save()
-                                    break
 
-                            return JsonResponse({'message': 'Pago almacenado exitosamente'}, status=200, safe=False)
-                        
+
                         else:
-                            # El pago debe ser rechazado porque es superior a la deuda.
-                            print('El pago debe ser rechazado porque es superior a la deuda.')
-                            payments_loan_detail = balance
-                            loan.status = 2
-                            # Guardar los cambios en la base de datos
-                            loan.save()
-                            return JsonResponse({'message': 'El pago debe ser rechazado porque es superior a la deuda.'}, status=200, safe=False)
-                        
-                    else:
-                        return JsonResponse({'message': 'El pago es rechazado porque el cliente no tiene prestamos activos'}, status=400)
-                    
-            else:
-                return JsonResponse({'error': 'El cliente no existe.'}, status=400)
+                            print('El credito ya esta cerrado.')
 
+                        if total_amount <= 0:
+                            print('Ya no hay dinero para seguir abonando.')
+                            break
+
+                return JsonResponse({'message': 'Pago almacenado exitosamente'}, status=200, safe=False)                
+
+
+ 
     except Customers.DoesNotExist as e:
             return JsonResponse({'error': 'El cliente no existe'}, status=500)
         
     except Exception as e:
-            return JsonResponse({'error': str(e)}, status=500)
+            return JsonResponse({'error':{e}}, status=500)
     
 def get_payments_by_customer(request):
 
@@ -122,32 +103,46 @@ def get_payments_by_customer(request):
             for loan in loans:
                 id_loan = loan.id
                 external_id_loan = loan.external_id
+                loan_amount = loan.amount
 
                 # Obtener todos los préstamos asociados al cliente
                 payments_info = PaymentsDetail.objects.filter(loan_id=id_loan)
+                print(payments_info)
 
-                # Obtener todos los préstamos asociados al cliente
-                payment_id = Payments.objects.filter(id=payments_info[0].payment_id.id)
-                actual_payment_id = payment_id[0].external_id
-                actual_payment_status = payment_id[0].status
 
+                
                 for payment_info in payments_info:
-                    payment_detail  = {
-                        'payment_detail_id': payment_info.payment_id.external_id,
-                        'amount':float(payment_info.amount),
-                        'payment_date':payment_info.created_at,
-                    }
-                                    
-                payment_customer  = {
-                        'customer_id': id_customer.external_id,
-                        'loan_id': external_id_loan,
-                        'payment_id': actual_payment_id,
-                        'total_amount':float(payment_info.amount),
-                        'status': actual_payment_status,
-                        'payment_detail': payment_detail,
-                    }
+                    # Obtener todos los préstamos asociados al cliente
+                    payments_id = Payments.objects.filter(id=payment_info.payment_id.id)
 
-                payments_customer.append(payment_customer)
+
+                    for payment_id in payments_id:
+                        payments_detail = []
+                        actual_payment_id = payment_id.external_id
+                        actual_payment_status = payment_id.status
+                        actual_payment_amount = payment_id.total_amount
+                        
+                        print('El grande',actual_payment_id)
+                        print('El micro',payment_info.payment_id.external_id)
+
+                        if actual_payment_id == payment_info.payment_id.external_id:
+                            payment_detail  = {
+                                'payment_detail_id': payment_info.payment_id.external_id,
+                                'amount':float(payment_info.amount),
+                                'payment_date':payment_info.created_at,
+                            }
+                            payments_detail.append(payment_detail)
+                                        
+                        payment_customer  = {
+                                'customer_id': id_customer.external_id,
+                                'loan_id': external_id_loan,
+                                'payment_id': actual_payment_id,
+                                'total_amount':float(actual_payment_amount),
+                                'status': actual_payment_status,
+                                'payment_detail': payments_detail,
+                            }
+
+                        payments_customer.append(payment_customer)
 
 
             if payments_customer != []:
